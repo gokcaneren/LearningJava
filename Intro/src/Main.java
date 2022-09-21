2fe09cacae663a7779a52a9af1840c9ba89d35d1
gokcaneren <gokcann01@gmail.com>
2022-09-21 23:03:41 +0300

    Variables

--- Intro/src/Main.java
+++ Intro/src/Main.java
@@ -3,7 +3,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello Java");
+		System.out.println("Hello Java");
+		
+		System.out.print("Going to learn Java Spring Boot!");
+		
 	}
 
 }
---
+++ Variables/src/Main.java
@@ -0,0 +1,36 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name="Gokcan";
+		System.out.println(name);
+		
+		int age=27;
+		System.out.println(age);
+		
+		System.out.println(name + " " +age);
+		
+		char c='E';
+		
+		System.out.println(c);
+		
+		float myNum=3.43f;
+		
+		System.out.println(myNum);
+		
+		double yourNum=3.13;
+		
+		System.out.println(yourNum);
+		
+		boolean isHeYoung=true;
+		
+		System.out.println(isHeYoung);
+		
+		isHeYoung=false;
+		
+		System.out.println(isHeYoung);
+	}
+
+}
