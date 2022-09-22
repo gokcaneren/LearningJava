24b071ee66ac452dfa8149295fc02d2616713415
gokcaneren <gokcann01@gmail.com>
2022-09-22 20:40:23 +0300

    Switch

--- If..Else..Switch/src/Main.java
+++ If..Else..Switch/src/Main.java
@@ -24,13 +24,38 @@ public class Main {
 			System.out.println(name + " is dead man!");
 		}
 		*/
-		
+		/*
 		int age=38;
 		
 		String result=(age<46) ? "Young":"Old";
 		
 		System.out.println(result);
+		*/
 		
+		int day = 7;
+		switch (day) {
+		  case 1:
+		    System.out.println("Monday");
+		    break;
+		  case 2:
+		    System.out.println("Tuesday");
+		    break;
+		  case 3:
+		    System.out.println("Wednesday");
+		    break;
+		  case 4:
+		    System.out.println("Thursday");
+		    break;
+		  case 5:
+		    System.out.println("Friday");
+		    break;
+		  case 6:
+		    System.out.println("Saturday");
+		    break;
+		  case 7:
+		    System.out.println("Sunday");
+		    break;
+		}
 	}
 
 }
