13f5b674193c0192317e714b402d961413d4e323
gokcaneren <gokcann01@gmail.com>
2022-09-22 21:20:01 +0300

    Loops

---
+++ Loops/src/Main.java
@@ -0,0 +1,39 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// While Loop
+		/*
+		int i=0;
+		while (i<10) {
+			System.out.println(i);
+			i++;
+		}
+		*/
+		
+		// Do while
+		/*
+		int i=0;
+		do {
+			System.out.println(i);
+			i++;
+		} while (i<10);
+		*/
+		
+		// For
+		/*
+		for (int i = 0; i <= 7; i++) {
+			System.out.println(i);
+		}
+		*/
+		
+		// For Each
+		String[] brands= {"Samsung","Apple","Xiaomi","Oppo","Huawei"};
+		for (String brand : brands) {
+			System.out.println(brand);
+		}
+	}
+
+}
