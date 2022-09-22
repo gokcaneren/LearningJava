e775adc8675c2dd8a58e0480b7ba97ab15b9226c
gokcaneren <gokcann01@gmail.com>
2022-09-22 20:36:56 +0300

    if..else

---
+++ If..Else..Switch/src/Main.java
@@ -0,0 +1,36 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*int a=13;
+		int b=13;
+		
+		if (a>b) {
+			System.out.println("a is bigger than b");
+		}else if (a<b) {
+			System.out.println("b is bigger than a");
+		}else {
+			System.out.println("a and b is equal");
+		}*/
+		/*
+		String name="John Wick";
+		boolean isAlive=true;
+		
+		if (isAlive==true) {
+			System.out.println(name + " is coming for you!");
+		}else {
+			System.out.println(name + " is dead man!");
+		}
+		*/
+		
+		int age=38;
+		
+		String result=(age<46) ? "Young":"Old";
+		
+		System.out.println(result);
+		
+	}
+
+}
