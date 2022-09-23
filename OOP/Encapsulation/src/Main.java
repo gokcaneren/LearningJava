f151f6f5965b6c1fb6430a269726403a85ff9318
gokcaneren <gokcann01@gmail.com>
2022-09-23 20:49:41 +0300

    Encapsulation

---
+++ OOP/Encapsulation/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Product product=new Product();
+		
+		product.setId(1);
+		product.setName("sss");
+		
+		System.out.println(product.getId()+product.getName());
+	}
+
+}
