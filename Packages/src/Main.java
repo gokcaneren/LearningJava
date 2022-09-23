09046abf523fe3dbd3e34563e24e10a5db5c856e
gokcaneren <gokcann01@gmail.com>
2022-09-23 23:18:59 +0300

    Packages

---
+++ Packages/src/Main.java
@@ -0,0 +1,15 @@
+import math.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Operations sumOperation=new Operations();
+		System.out.println(sumOperation.sum(5, 7));
+		
+		Operations subOperation=new Operations();
+		System.out.println(subOperation.subtraction(10, 3));
+	}
+
+}
---
+++ Packages/src/math/Operations.java
@@ -0,0 +1,12 @@
+package math;
+
+public class Operations {
+
+	public int sum(int num1, int num2) {
+		return num1+num2;
+	}
+	
+	public int subtraction(int num1, int num2) {
+		return num1-num2;
+	}
+}
