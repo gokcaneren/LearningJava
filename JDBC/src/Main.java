3883290599dc0f7c8854f64bc7f87a412fe7af0a
gokcaneren <gokcann01@gmail.com>
2022-09-28 23:19:49 +0300

    DbHelper added

--- JDBC/src/Main.java
+++ JDBC/src/Main.java
@@ -1,23 +1,23 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main {
 
-	static String userName="root";
-	static String password="gok12345";
-	static String dbUrl="jdbc:mysql://localhost:3306/ETrade";
 	
 	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
 
 		Connection connection=null;
+		DbHelper dbHelper=new DbHelper();
 		
 		try {
-			connection=DriverManager.getConnection(dbUrl, userName, password);
+			connection=dbHelper.connectDatabase();
 			System.out.println("Succesfully connected to database!");
+			
+			connection.nativeSQL("insert into product (Name, Stock) values ('Oppo Reno6', 13)");
+			
 		} catch (SQLException ex) {
-			System.out.println(ex.getMessage());
+			dbHelper.showErrorMessage(ex);
 		} finally {
 			connection.close();
 		}
